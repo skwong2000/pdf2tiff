ebde4fe409054afae00fc07674b75d7dfca5a380
Clive Wong <skwong2000@hotmail.com>
2020-03-13 20:11:10 +0800

    first build in eclipse

--- PDF2TIFF/src/main/java/converter/PDF2TIFF/PDF2TIFF_Converter.java
+++ PDF2TIFF/src/main/java/converter/PDF2TIFF/PDF2TIFF_Converter.java
@@ -67,7 +67,9 @@ public class PDF2TIFF_Converter {
 	
 	public static void main(String[] args) {
 		try {
-			PDF2TIFF_Converter.convert("test/Sample.pdf", "test/Sample.tiff");
+			String pdfFile = "C:\\projects\\clive\\eclipse-workspace\\pdf2tiff\\PDF2TIFF\\temp\\efx.pdf";
+			String tifFile = "C:\\projects\\clive\\eclipse-workspace\\pdf2tiff\\PDF2TIFF\\temp\\efx.tif";			
+			PDF2TIFF_Converter.convert(pdfFile, tifFile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
